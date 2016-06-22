c1c12d04e7cf307938909d6789b0c07e7b4802e3
Ekaterina Stambolieva <estambolieva@gmail.com>
2016-06-22 17:56:15 +0100

    quick sort with first element of array as pivot

--- week2/QuickSort.py
+++ week2/QuickSort.py
@@ -3,13 +3,11 @@ def quickSort(a,start, end):
     # base case
     if ((end - start) == 1):
         return a
-    # select pivot - pivot index is in [start, end)
-    pivot_ind = random.randrange(start, end)
-    pivot = a[pivot_ind]
+    # select pivot - pivot is the first element of the array
+    pivot = a[start]
     # partition around pivot
     i = start
-    j_initial = start
-    for j in range(j_initial, end):
+    for j in range(start, end):
         #check is there is a swap
         if (a[j] < pivot):
             # swap
@@ -18,7 +16,7 @@ def quickSort(a,start, end):
                 a[j] = a[i]
                 a[i] = temp
             # advance indices
-            i += 1
+            i = min(i+1, end-1)
     # put pivot in rightful position
     new_pivot_ind = a.index(pivot)
     temp_pivot = a[i]
@@ -30,8 +28,6 @@ def quickSort(a,start, end):
     if(end-(i+1) >= 1):
         quickSort(a, i + 1, end)
 
-#a = [3,8,2,5,1,4,7,6]
-#a = [10,9,7,8,5,6,4,3,2,1]
-a = [1,2,3,4,5,6]
+a = [3,8,2,5,1,4,7,6]
 quickSort(a, 0, len(a))
 print "sorted array is -> " + str(a)
