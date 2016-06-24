1d45daecb3f729cee5a4748409d6101f8dea2e24
Ekaterina Stambolieva <estambolieva@gmail.com>
2016-06-24 12:21:01 +0100

    quick sort implementation exactly as shown on lectures. problem 1 solved - number of comparisons when the pivot is the first element of the array

--- week2/QuickSort.py
+++ week2/QuickSort.py
@@ -1,13 +1,15 @@
-import random
+no_comparisons = 0
+
 def quickSort(a,start, end):
+    global no_comparisons, first_pass
     # base case
     if ((end - start) == 1):
         return a
     # select pivot - pivot is the first element of the array
     pivot = a[start]
     # partition around pivot
-    i = start
-    for j in range(start, end):
+    i = start + 1
+    for j in range(start + 1, end):
         #check is there is a swap
         if (a[j] < pivot):
             # swap
@@ -16,18 +18,29 @@ def quickSort(a,start, end):
                 a[j] = a[i]
                 a[i] = temp
             # advance indices
-            i = min(i+1, end-1)
+            i = i+1
     # put pivot in rightful position
     new_pivot_ind = a.index(pivot)
-    temp_pivot = a[i]
-    a[i] = a[new_pivot_ind]
+    temp_pivot = a[i - 1]
+    a[i - 1] = a[new_pivot_ind]
     a[new_pivot_ind] = temp_pivot
     # recursive calls
-    if((i-start) >= 1):
-        quickSort(a, start, i)
-    if(end-(i+1) >= 1):
-        quickSort(a, i + 1, end)
+    if((i-start-2) >= 1):
+        no_comparisons += (i - start - 2)
+        quickSort(a, start, i-1)
+    if(end - i >= 1):
+        no_comparisons += (end - i - 1)
+        quickSort(a, i, end)
 
-a = [3,8,2,5,1,4,7,6]
-quickSort(a, 0, len(a))
-print "sorted array is -> " + str(a)
+fname = "quick_sorted_array.txt"
+with open(fname) as f:
+    content = f.readlines()
+    content = map(int, content)
+#content = [10, 1, 8, 2, 7, 3, 6, 4, 5, 20, 12]
+#content = [3,8,2,5,1,4,7,6]
+#content = [5,4,3,2,1]
+n = len(content)
+quickSort(content, 0, n)
+no_comparisons += (n - 1)
+#print "sorted array is -> " + str(content)
+print "number of comparisons -> " + str(no_comparisons)
