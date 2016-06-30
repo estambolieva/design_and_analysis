957ca8f484555061eacfe21eabe29109632e0387
Ekaterina Stambolieva <estambolieva@gmail.com>
2016-06-30 10:48:29 +0100

    added an image with the three examples thoroughly tested

---
+++ week3/rMinCut.py
@@ -0,0 +1,53 @@
+import random
+
+def rMinCut(nodes, edges):
+    # until we have only two nodes left
+    while (len(nodes) > 2):
+        print "_______________________"
+        # select at random an edge (u,v) from the list of edges
+        edge_ind = random.randrange(0, len(nodes))
+        uv = edges[edge_ind]
+        print "random edge (u,v) is -> " + str(uv)
+        u = uv[0]
+        v = uv[1]
+        node_to_remove = v
+        # replace all v-s with u-s in the list of edges
+        i = 0
+        while (i < len(edges)):
+            u1v1 = edges[i]
+            u1 = u1v1[0]
+            v1 = u1v1[1]
+            #print "v, u1 and v1 are -> (" + str(v) + ") and (" + str(u1) + ":" + str(v1) + ")"
+            if (u1 == v):
+                edges[i] = (u, v1)
+                # remove self-loops, e.g. (u,u)
+                if (u == v1):
+                    edges.remove(edges[i])
+                    i -= 1
+            if (v1 == v):
+                edges[i] = (u1, u)
+                # remove self-loops, e.g. (u,u)
+                if (u1 == u):
+                    edges.remove(edges[i])
+                    i -= 1
+            i += 1
+        # remove u from the list of nodes
+        #print "leftover edges are " + str(edges)
+        print "removing node -> " + str(node_to_remove) + " from array " + str(nodes)
+        nodes.remove(node_to_remove)
+        print "current nodes left are -> " + str(nodes)
+
+    return (len(edges)/2)
+
+#example 1
+#nodes = [1,2,3,4]
+#edges = [(1,2), (1,4), (2,1), (2,3), (2,4), (3,2), (3,4), (4,1), (4,2), (4,3)]
+
+#example 2
+#nodes = [1,2,3,4,5]
+#edges = [(1,2), (1,5), (2,1), (2,3), (2,4), (2,5), (3,2), (3,4), (3,5), (4,2), (4,3), (4,5), (5,1), (5,2), (5,3), (5,4)]
+
+#example 3
+nodes = [1,2,3,4,5,6,7,8]
+edges = [(1,2), (1,7), (1,8), (2,1), (2,3), (2,7), (2,8), (3,2), (3,4), (3,5), (3,6), (4,3), (4,5), (4,6), (5,3), (5,4), (5,6), (6,3), (6,4), (6,5), (6,7), (7,1), (7,2), (7,6), (7,8), (8,1), (8,2), (8,7)]
+print "min cut is -> " + str(rMinCut(nodes, edges))
